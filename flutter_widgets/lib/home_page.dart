8c3872eea83972c0dd631f2779cb4221529ed929
Mingriweiji-github <2822072760@qq.com>
2020-08-04 15:29:29 +0800

    flutter dialog

--- flutter_widgets/lib/home_page.dart
+++ flutter_widgets/lib/home_page.dart
@@ -146,10 +146,105 @@ class _HomePageState extends State<HomePage> {
                 );
               },
               child: Text("FlutterSpiderView"),
-            )
+            ),
+
+            FlatButton(
+              onPressed: () {
+                showConfirmDialog();
+//                showDialogWithAnimation();
+              },
+              child: Text("AlertDialog"),
+            ),
+
+
+
           ],
         ),
       ),
+    );
+  }
+
+  ///弹出对话窗
+  Future<bool> showConfirmDialog() {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("说明"),
+          titleTextStyle: TextStyle(fontSize: 16, color: Colors.grey), // 666666
+          content: Text("1实现代码很简单，不在赘述。\n2唯一需要注意的是我们是通过Navigator.of(context).pop(…)方法来关闭对话框的\n3这和路由返回的方式是一致的，并且都可以返回一个结果数据。现在，对话框我们已经构建好了，那么如何将它弹出来呢？\n4还有对话框返回的数据应如何被接收呢？这些问题的答案都在showDialog()方法中。"),
+          contentTextStyle: TextStyle(fontSize: 12, color: Colors.grey), // 666666
+          actions: <Widget>[
+            Container(
+              child: FlatButton(
+                child: Text("确定", style: TextStyle(fontSize: 16, color: Colors.blue)),
+                onPressed: () => Navigator.of(context).pop(), // 关闭对话框
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void showDialogWithAnimation() {
+    _showCustomDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("提示"),
+          titleTextStyle: TextStyle(fontSize: 16, color: Colors.grey),
+          content: Text("1实现代码很简单，不在赘述。\n2唯一需要注意的是我们是通过Navigator.of(context).pop(…)方法来关闭对话框的\n3这和路由返回的方式是一致的，并且都可以返回一个结果数据。现在，对话框我们已经构建好了，那么如何将它弹出来呢？\n4还有对话框返回的数据应如何被接收呢？这些问题的答案都在showDialog()方法中。"),
+          contentTextStyle: TextStyle(fontSize: 12, color: Colors.grey), // 666666// 666666
+          actions: <Widget>[
+            FlatButton(
+              child: Text("确定"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  Future<T> _showCustomDialog<T>({
+    @required BuildContext context,
+    bool barrierDismissible = true,
+    WidgetBuilder builder,
+  }) {
+    final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+    return showGeneralDialog(
+      context: context,
+      pageBuilder: (BuildContext buildContext, Animation<double> animation,
+          Animation<double> secondaryAnimation) {
+        final Widget pageChild = Builder(builder: builder);
+        return SafeArea(
+          child: Builder(builder: (BuildContext context) {
+            return theme != null
+                ? Theme(data: theme, child: pageChild)
+                : pageChild;
+          }),
+        );
+      },
+      barrierDismissible: barrierDismissible,
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      barrierColor: Colors.black54, // 自定义遮罩颜色
+      transitionDuration: const Duration(milliseconds: 150),
+      transitionBuilder: _buildMaterialDialogTransitions,
+    );
+  }
+
+  Widget _buildMaterialDialogTransitions(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    // 使用缩放动画
+    return ScaleTransition(
+      scale: CurvedAnimation(
+        parent: animation,
+        curve: Curves.easeOut,
+      ),
+      child: child,
     );
   }
 }
